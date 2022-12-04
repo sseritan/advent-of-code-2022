65cca8cbd461b168d9915de76913b2fd9bc2373e
sseritan <sseritan@gmail.com>
2022-12-03 21:19:38 -0800

    Day 4, Part 2

--- day4/puzzle.py
+++ day4/puzzle.py
@@ -3,7 +3,33 @@
 
 import sys
 
-def find_nested_interval(fname: str = 'input.txt') -> int:
+def find_nested_interval(fname: str = 'input.txt') -> None:
+    with open(fname, 'r') as f:
+        lines = f.readlines()
+
+    num_nested = 0
+    num_overlaps = 0
+    for i, l in enumerate(lines):
+        interval1, interval2 = l[:-1].split(',') if r'\n' in l else l.split(',')
+        start1, end1 = interval1.split('-')
+        start2, end2 = interval2.split('-')
+        start1 = int(start1)
+        end1 = int(end1)
+        start2 = int(start2)
+        end2 = int(end2)
+
+        if (start1 >= start2 and end1 <= end2) \
+            or (start2 >= start1 and end2 <= end1):
+            num_nested += 1
+        
+        if (start1 >= start2 and start1 <= end2) \
+            or (start2 >= start1 and start2 <= end1):
+            num_overlaps += 1
+    
+    print(f'Number of nested intervals is {num_nested}')
+    print(f'Number of overlapping intervals is {num_overlaps}')
+
+def find_overlapping_interval(fname: str = 'input.txt') -> int:
     with open(fname, 'r') as f:
         lines = f.readlines()
 
