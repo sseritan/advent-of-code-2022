6c799b83fc9cd2e4b7ab5ec514d7a386940c32ba
sseritan <sseritan@gmail.com>
2022-12-09 11:31:48 -0800

    Day 8, Part 2

--- day08/puzzle.py
+++ day08/puzzle.py
@@ -55,8 +55,49 @@ def count_visible_trees(fname: str = 'input.txt'):
     #print(visible_trees)
     print(f'Visible trees: {len(visible_trees)}')
 
+def scenic_tree_score(fname: str = 'input.txt'):
+    with open(fname, 'r') as f:
+        lines = f.readlines()
+    
+    grid = [[int(c) for c in l if c != '\n'] for l in lines]
+    num_rows = len(grid)
+    num_cols = len(grid[0])
+
+    def check_direction(row, col, range, x=True):
+        num_trees = 0
+        height = grid[row][col]
+        
+        for i in range:
+            num_trees += 1
+            if (x and grid[i][col] >= height) or (not x and grid[row][i] >= height):
+                break
+        
+        return num_trees
+
+    max_score = -1
+    max_position = None
+    max_visibilities = None
+    for row in range(num_rows):
+        for col in range(num_cols):
+            # Check each direction
+            left = check_direction(row, col, range(row-1, -1, -1), True)
+            right = check_direction(row, col, range(row+1, num_rows), True)
+            up = check_direction(row, col, range(col-1, -1, -1), False)
+            down = check_direction(row, col, range(col+1, num_cols), False)
+
+            score = left*right*up*down
+
+            if score > max_score:
+                max_score = score
+                max_position = (row, col)
+                max_visibilities = {'l': left, 'r': right, 'u': up, 'd': down}
+
+    print(f'Max scenic score is: {max_score} (at position {max_position} with visibilities {max_visibilities}')                
+
 if __name__ == '__main__':
     if len(sys.argv) == 2:
         count_visible_trees(sys.argv[1])
+        scenic_tree_score(sys.argv[1])
     else:
         count_visible_trees()
+        scenic_tree_score()
