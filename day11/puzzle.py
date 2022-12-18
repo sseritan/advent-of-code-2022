527515d4e9224f85212c54a1f1c1c13c04a7d438
sseritan <sseritan@gmail.com>
2022-12-17 23:09:48 -0800

    Day 11, Part 2 done

--- day11/puzzle.py
+++ day11/puzzle.py
@@ -1,11 +1,11 @@
 """Day 11
 For easy parsing, assume input has one empty line at the start
-python puzzle.py example.py 20 3 -> Part 1 example, expect 10605
-python puzzle.py input.py 20 3 -> Part 1 answer
-python puzzle.py example.py 20 1 -> Part 2 example,    expect 10917
-python puzzle.py example.py 1000 1 -> Part 2 example,  expect 27019168
-python puzzle.py example.py 5000 1 -> Part 2 example,  expect 677950000
-python puzzle.py example.py 10000 1 -> Part 2 example, expect 2713310158
+python puzzle.py example.py 20 1 -> Part 1 example, expect 10605
+python puzzle.py input.py 20 1 -> Part 1 answer (78678)
+python puzzle.py example.py 20 -> Part 2 example,    expect 10197
+python puzzle.py example.py 1000 -> Part 2 example,  expect 27019168
+python puzzle.py example.py 5000 -> Part 2 example,  expect 677950000
+python puzzle.py example.py 10000 -> Part 2 example, expect 2713310158
 """
 
 from typing import Iterable
@@ -14,10 +14,12 @@ import sys
 MONKEYS = []
 
 class Monkey(object):
-    def __init__(self, lines: Iterable[str], worry_divisor: int = 1) -> None:
+    def __init__(self, lines: Iterable[str], index: int) -> None:
+        self.index = index
+
         # Starting items
-        items = lines[0].split(': ')[1]
-        self.items = [int(item) for item in items.split(', ')]
+        self.items = [int(item) for item in lines[0].split(': ')[1].split(', ')]
+        self.remainders = None
 
         # Operation
         self.op = lines[1].split()[-2]
@@ -25,16 +27,15 @@ class Monkey(object):
         self.op_value = 'old' if lines[1].endswith('old\n') else int(lines[1].split()[-1])
 
         # Test
-        self.div_value = int(lines[2].split()[-1])
+        self.test_divisor = int(lines[2].split()[-1])
 
         # True/false
         self.true_monkey = int(lines[3].split()[-1])
         self.false_monkey = int(lines[4].split()[-1])
-    
-        self.worry_divisor = worry_divisor
+
         self.num_inspects = 0
 
-    def take_turn(self) -> None:
+    def take_part1_turn(self) -> None:
         for item in self.items:
             # Inspect/update worry
             op_val = item if self.op_value == 'old' else self.op_value
@@ -44,10 +45,10 @@ class Monkey(object):
                 item += op_val
             
             # Integer divide by 3 for surviving inspection
-            item //= self.worry_divisor
+            item //= 3
 
             # Test and give item
-            if item%self.div_value == 0:
+            if item%self.test_divisor == 0:
                 MONKEYS[self.true_monkey].items.append(item)
             else:
                 MONKEYS[self.false_monkey].items.append(item)
@@ -56,24 +57,56 @@ class Monkey(object):
         
         # Empty queue
         self.items = []
+    
+    def take_part2_turn(self) -> None:        
+        for item_rems in self.remainders:
+            # Inspect/update worry
+            new_rems = []
+            for i, rem in enumerate(item_rems):
+                op_val = rem if self.op_value == 'old' else self.op_value
+                if self.op == '*':
+                    rem *= op_val
+                else:
+                    rem += op_val
+                new_rems.append(rem%MONKEYS[i].test_divisor)
 
-def compute_monkey_business(fname: str = 'input.txt', num_rounds: int = 10000, num_active: int = 2,
-    worry_divisor: int = 1) -> None:
+            # Test and give
+            if new_rems[self.index] == 0:
+                MONKEYS[self.true_monkey].remainders.append(new_rems)
+            else:
+                MONKEYS[self.false_monkey].remainders.append(new_rems)
+            
+            self.num_inspects += 1
+        
+        # Empty queue
+        self.remainders = []
+
+def compute_monkey_business(fname: str = 'input.txt', num_rounds: int = 10000, part: int = 2) -> None:
+    num_active = 2 # Hardcoded
+
     with open(fname, 'r') as f:
         lines = f.readlines()
 
     num_monkeys = len(lines) // 7
     for i in range(num_monkeys):
-        MONKEYS.append(Monkey(lines[7*i+2:7*(i+1)], worry_divisor))
+        MONKEYS.append(Monkey(lines[7*i+2:7*(i+1)], i))
+    
+    if part == 2:
+        for monkey in MONKEYS:
+            monkey.remainders = [[item%m.test_divisor for m in MONKEYS] for item in monkey.items]
 
     # Do all monkey business
     for i in range(num_rounds):
         for monkey in MONKEYS:
-            monkey.take_turn()
+            if part == 1:
+                monkey.take_part1_turn()
+            else:
+                monkey.take_part2_turn()
     
     # Sort and get top monkeys
     activity = sorted([m.num_inspects for m in MONKEYS], reverse=True)
     
+    # Compute monkey business
     monkey_business = 1
     for i in range(num_active):
         monkey_business *= activity[i]
@@ -81,7 +114,9 @@ def compute_monkey_business(fname: str = 'input.txt', num_rounds: int = 10000, n
     print(f'Monkey business for top {num_active} monkeys after {num_rounds} rounds: {monkey_business}')
 
 if __name__ == '__main__':
-    if len(sys.argv) == 4:
-        compute_monkey_business(sys.argv[1], num_rounds=int(sys.argv[2]), worry_divisor=int(sys.argv[3]))
+    if len(sys.argv) == 3:
+        compute_monkey_business(sys.argv[1], num_rounds=int(sys.argv[2]))
+    elif len(sys.argv) == 4:
+        compute_monkey_business(sys.argv[1], num_rounds=int(sys.argv[2]), part=int(sys.argv[3]))
     else:
         compute_monkey_business()
